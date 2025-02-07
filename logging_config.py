6f275a42ccb7c1723697feebac6285a346e5fbbc
Mohamed <mouss.mo.ke@gmail.com>
2025-02-07 23:18:43 +0100

    logging file

--- logging_config.py
+++ logging_config.py
@@ -1,7 +1,9 @@
 import logging
+from utils.utils import get_today
 
 logging.basicConfig(
     level=logging.INFO,  
+    filename="logs_{}.txt".format(get_today().replace(' ', '')),
     format="%(asctime)s - %(name)s - %(message)s",
     handlers=[
         logging.FileHandler("app.log"),  
