71e4d5c41dabdd7ba95699f10e50f6a3643e8ec3
Mohamed <mouss.mo.ke@gmail.com>
2025-02-08 01:59:24 +0100

    s3 retrieval bug fix

--- logging_config.py
+++ logging_config.py
@@ -4,9 +4,5 @@ from utils.utils import get_today
 logging.basicConfig(
     level=logging.INFO,  
     filename="logs_{}.txt".format(get_today().replace(' ', '')),
-    format="%(asctime)s - %(name)s - %(message)s",
-    handlers=[
-        logging.FileHandler("app.log"),  
-        logging.StreamHandler()         
-    ]
+    format="%(asctime)s - %(name)s - %(message)s"
 )
